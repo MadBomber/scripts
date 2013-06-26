1a13b2a631b61fb59250044e43719c61735bc791
Dewayne VanHoozer <dvanhoozer@gmail.com>
2013-06-26 16:23:26 -0500

    added fake.rb based upon the 'wordy' gem

---
+++ fake.rb
@@ -0,0 +1,23 @@
+#! /usr/bin/env ruby
+# encoding: UTF-8
+
+require "wordy"
+
+m = ARGV.shift || "body"
+
+if m.to_i > 0
+  c = m.to_i
+  m = ARGV.shift || "body"
+else
+  c = 1
+end
+
+if m && Wordy.respond_to?(m)
+  puts Wordy.send(m, c)
+elsif m == "-v"
+  puts "Wordy v#{Wordy::VERSION}"
+else
+  puts "Invalid option. Please try one of the following..."
+  puts "=> word words sentence sentences paragraph paragraphs body"
+  puts "   Use can also use the form:  3 words; 2 paragraphs; etc."
+end
