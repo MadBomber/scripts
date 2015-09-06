c8c13349cde3f921647dfb6547d7a12eae4395fb
Dewayne VanHoozer <dvanhoozer@42six.com>
2015-09-05 23:31:11 -0500

    removed ncurses in favor of IO/console to get max_columns

--- describe_gems.rb
+++ describe_gems.rb
@@ -5,6 +5,9 @@
 ##  Desc: Display the summary and description for a set of gems
 #
 
+require 'IO/console'
+MAX_COLUMNS = IO.console.winsize.last - 1
+
 usage_str = <<EOS
 
 Usage: #{$0.split('/').last} gem+
@@ -22,23 +25,30 @@ require 'pp'
 
 require 'yaml'
 require 'systemu'
-require 'ffi-ncurses'
 
 # FIXME: make word_wrap a gem
 require 'word_wrap'
-include WordWrap
-
-FFI::NCurses.initscr
-set_default_width(FFI::NCurses.COLS)
-FFI::NCurses.endwin
+require 'word_wrap/core_ext'
 
 
+def wrap_with_label(a_label, a_desc)
+  spacer = " "*a_label.size
+  desc_array = a_desc.fit(MAX_COLUMNS - a_label.size).split("\n")
+  desc_array[0]= a_label + desc_array.first
+  if desc_array.size > 1
+    (1..desc_array.size-1).each do |x|
+      desc_array[x] = spacer + desc_array[x]
+    end
+  end
+
+  return desc_array.join("\n")
+end
 
 gems = ARGV
 
 gems.each do |gem|
-  spec = YAML.load(systemu("gem spec #{gem}")[1])
-  
+  spec = YAML.load(`gem spec #{gem}`)
+
   unless spec
     puts "\n#{gem} -- ERROR no such gem installed"
     next
@@ -47,19 +57,26 @@ gems.each do |gem|
   puts "\n#{gem} (#{spec.version}) #{spec.homepage}"
 
   summary = spec.summary
-  unless summary.nil? or 0 == summary.strip.length
-    puts
+  unless summary.nil? || 0 == summary.strip.length
     puts wrap_with_label("  Summary:     ", "#{summary}" )
   end
 
   description = spec.description
-  unless description.nil? or 0 == description.strip.length
-    puts
+  unless description.nil? || 
+         0 == description.strip.length ||
+         summary == description
     puts wrap_with_label("  Description: ", "#{description}" )
   end
   
+  depends = spec.dependencies
+  unless depends.empty?
+    puts wrap_with_label(
+      "   Depends on: ", 
+      depends.map{|d| d.name}.join(', ')
+    )
+  end
+
   puts
-#  pp spec.methods.sort
 end
 
 
