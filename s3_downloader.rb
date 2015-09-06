670d367be2ef2bcb119969c677f629eb89217891
Dewayne VanHoozer <dvanhoozer@gmail.com>
2015-09-06 16:03:47 -0500

    old stuff; the uploader is just a stub

---
+++ s3_downloader.rb
@@ -0,0 +1,281 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+##########################################################
+###
+##  File: s3_downloader.rb
+##  Desc: Download files from an AWS/S3 bucket
+##  By:   Dewayne VanHoozer (dvanhoozer@gmail.com)
+#
+
+require 'debug_me'
+include DebugMe
+
+require 'pathname'
+require 'nenv'
+require 'slop'
+
+module Slop
+  class PathOption < Option
+    def call(value)
+      Pathname.new(value)
+    end
+  end
+  class PathsOption < ArrayOption
+    def finish(opts)
+      self.value = value.map { |f| Pathname.new(f) }
+    end
+  end
+end # module Slop
+
+require 'aws-sdk'
+
+me        = Pathname.new(__FILE__).realpath
+my_dir    = me.parent
+my_name   = me.basename.to_s
+
+$options = {
+  version:        '0.0.1',
+  arguments:      [],     # whats left after options and parameters are extracted
+  verbose:        false,
+  debug:          false,
+  list:           false,
+  files:          [],
+  download_dir:   Pathname.new(Nenv.home+'/Downloads'),
+  bucket:         Nenv.aws_s3_bucket || '',
+  s3: {
+        access_key_id:      Nenv.aws_access_key_id     || '',
+        secret_access_key:  Nenv.aws_secret_access_key || ''
+      },
+
+}
+
+def verbose?
+  $options[:verbose]
+end
+
+def debug?
+  $options[:debug]
+end
+
+def list?
+  $options[:list]
+end
+
+HELP = <<ENDHELP
+
+Important:
+
+  The -l, --list parameter surpresses downloads.
+
+  To see a list of all buckets use '-l -b /'
+
+  Wildcards '*' and '?' can be used in filenames if
+  the filename is surrounded in quotes.
+
+  Example: -b bucket_name -f '*.txt'
+
+  Use of wildcards may result in many files being
+  downloaded.
+
+  When specifing the filenames do not forget to
+  include the folder name when the file resides
+  within a folder inside the bucket.
+
+ENDHELP
+
+opts = Slop.parse do |o|
+  o.banner = "\nDownload files from an AWS/S3 bucket"
+  o.separator "\nUsage: #{my_name} [options] parameters"
+  o.separator "\nWhere:"
+  o.separator "  options"
+
+  o.on '-h', '--help', 'show this message' do
+    puts o
+    puts HELP if defined?(HELP)
+    exit
+  end
+
+  o.bool '-v', '--verbose', 'enable verbose mode'
+  o.bool '-d', '--debug',   'enable debug mode'
+
+  o.separator "\n  parameters"
+
+  o.bool   '-l', '--list',   'list contents of bucket'
+  o.string '-b', '--bucket', 'an S3 bucket name', default: $options[:bucket]
+  o.array  '-f', '--files',  'one or more files to download'
+
+  o.on '--version', "print the version: #{$options[:version]}" do
+    puts $options[:version]
+    exit
+  end
+
+  o.separator "\n"
+end
+
+
+$options.merge!(opts.to_hash)
+$options[:arguments] = opts.arguments
+
+# Display the usage info
+if  ARGV.empty?
+  puts opts
+  puts HELP if defined?(HELP)
+  exit
+end
+
+# Check command line for Problems with Parameters
+$errors   = []
+$warnings = []
+
+# Display global warnings and errors arrays and exit if necessary
+def abort_if_errors
+  unless $warnings.empty?
+    STDERR.puts
+    STDERR.puts "The following warnings were generated:"
+    STDERR.puts
+    $warnings.each do |w|
+      STDERR.puts "\tWarning: #{w}"
+    end
+    STDERR.print "\nAbort program? (y/N) "
+    answer = (gets).chomp.strip.downcase
+    $errors << "Aborted by user" if answer.size>0 && 'y' == answer[0]
+    $warnings = []
+  end
+  unless $errors.empty?
+    STDERR.puts
+    STDERR.puts "Correct the following errors and try again:"
+    STDERR.puts
+    $errors.each do |e|
+      STDERR.puts "\t#{e}"
+    end
+    STDERR.puts
+    exit(-1)
+  end
+end # def abort_if_errors
+
+
+credentials = Aws::Credentials.new(
+    $options[:s3][:access_key_id],
+    $options[:s3][:secret_access_key]
+)
+
+s3 = Aws::S3::Client.new(credentials: credentials)
+
+begin
+  bucket_accessor = s3.list_objects(bucket: $options[:bucket]) unless '/'==$options[:bucket]
+  buckets = s3.list_buckets()[:buckets]
+rescue Aws::S3::Errors::AccessDenied
+  $errors << "Access denied to bucket #{$options[:bucket]}"
+rescue Aws::S3::Errors::NoSuchBucket
+  $errors << "AWS Bad Bucket Name: #{$options[:bucket]}"
+rescue Aws::S3::Errors::SignatureDoesNotMatch => e
+  $errors << "AWS Connection Error: #{e}"
+end
+
+
+
+
+unless $options[:arguments].empty?
+  $errors << "Unknown arguments: #{$options[:arguments]}"
+end
+
+
+
+
+abort_if_errors
+
+
+######################################################
+# Local methods
+
+def get_filenames_in_bucket(a_bucket_accessor)
+  filenames = []
+  a_bucket_accessor.first[:contents].each do |rrr|
+    filenames << rrr[:key]
+  end
+  return filenames
+end
+
+
+######################################################
+# Main
+
+at_exit do
+  puts
+  puts "Done."
+  puts
+end
+
+pp $options  if debug?
+
+
+if $options[:bucket].nil? || '/' == $options[:bucket]
+  puts
+  puts '.....Creation Date.....  Bucket Name........................'
+  puts
+  buckets.each do |bucket|
+    print bucket[:creation_date].to_s + '  '
+    puts bucket[:name]
+  end
+
+  puts
+  puts
+  exit
+end
+
+if list?
+  # response = s3.list_objects(bucket: $options[:bucket])
+
+  puts '...Last Modification...  ..Size..  ...Name (aka :key)....
+  '
+  bucket_accessor.first[:contents].each do |rrr|
+    print rrr[:last_modified].to_s + '  '
+    printf( '% 8d  ', rrr[:size])
+    puts rrr[:key]
+  end
+  puts
+  puts
+  exit
+end
+
+# response = s3.list_objects(bucket: $options[:bucket])
+
+$options[:files].map! do |filename|
+  if filename.include?('*') || filename.include?('?')
+    fn_regex = filename.gsub('*','.*').gsub('?','.')
+    get_filenames_in_bucket(bucket_accessor).grep(Regexp.new(fn_regex))
+  else
+    filename
+  end
+end
+
+$options[:files].flatten!
+
+unless $options[:files].empty?
+
+  $options[:files].each do |filename|
+    path = $options[:download_dir] + filename
+    verbose? ? puts("Downloading #{$options[:bucket]}/#{filename} ...") : print('.')
+    File.open(path, 'wb') do |f|
+      reap = s3.get_object({
+                    bucket:   $options[:bucket],
+                    key:      filename },
+                    target:   f)
+    end
+  end
+
+else
+
+  STDERR.puts 'ERROR: No files were selected for download.'
+
+end # unless $options[:files].empty?
+
+
+
+
+
+
+
+
+
+
---
+++ s3_uploader.rb
@@ -0,0 +1,282 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+##########################################################
+###
+##  File: s3_uploader.rb
+##  Desc: Upload files to an AWS/S3 bucket
+##  By:   Dewayne VanHoozer (dvanhoozer@gmail.com)
+#
+
+# TODO: complete this stub
+
+require 'debug_me'
+include DebugMe
+
+require 'pathname'
+require 'hipchat_s3'
+
+me        = Pathname.new(__FILE__).realpath
+my_dir    = me.parent
+my_name   = me.basename.to_s
+
+$options = {
+  verbose:        false,
+  debug:          false,
+  notify:         true,
+  s3: {
+        access_key_id:      ENV['AWS_ACCESS_KEY_ID']     || 'unknown',
+        secret_access_key:  ENV['AWS_SECRET_ACCESS_KEY'] || 'unknown',
+        bucket:             ENV['AWS_S3_BUCKET']         || 'unknown'
+      },
+  hipchat: {
+        api_token:  ENV['HIPCHAT_TOKEN'] || 'unknown',
+        room_name:  ENV['HIPCHAT_ROOM']  || 'unknown'
+      }
+}
+
+
+def verbose?
+  $options[:verbose]
+end
+
+def debug?
+  $options[:debug]
+end
+
+def notify?
+  $options[:notify]
+end
+
+
+usage = <<EOS
+
+__file_description__
+
+Usage: #{my_name} [options] parameters
+
+Where:
+
+  options               Do This
+    -h or --help        Display this message
+    -v or --verbose     Display progress
+    -o or --output      Specifies the path to the output
+        out_filename      file.  The extname must be 'ics'
+                          Defaults to STDOUT
+
+  parameters            The parameters required by
+                        the program
+
+NOTE:
+
+  Something_imporatant
+
+EOS
+
+# Check command line for Problems with Parameters
+$errors   = []
+$warnings = []
+
+
+# Get the next ARGV parameter after param_index
+def get_next_parameter(param_index)
+  unless Fixnum == param_index.class
+    param_index = ARGV.find_index(param_index)
+  end
+  next_parameter = nil
+  if param_index+1 >= ARGV.size
+    $errors << "#{ARGV[param_index]} specified without parameter"
+  else
+    next_parameter = ARGV[param_index+1]
+    ARGV[param_index+1] = nil
+  end
+  ARGV[param_index] = nil
+  return next_parameter
+end # def get_next_parameter(param_index)
+
+
+# Get $options[:out_filename]
+def get_out_filename(param_index)
+  filename_str = get_next_parameter(param_index)
+  $options[:out_filename] = Pathname.new( filename_str ) unless filename_str.nil?
+end # def get_out_filename(param_index)
+
+
+# only gets valid files of one type
+def get_pathnames_from(an_array, extnames=['.json', '.txt', '.docx'])
+  an_array = [an_array] unless an_array.is_a? Array
+  extnames = [extnames] unless extnames.is_a? Array
+  extnames = extnames.map{|e| e.downcase}
+  file_array = []
+  an_array.each do |a|
+    pfn = Pathname.new(a)
+    if pfn.directory?
+      file_array << get_pathnames_from(pfn.children, extnames)
+    else
+      file_array << pfn if pfn.exist? && extnames.include?(pfn.extname.downcase)
+    end
+  end
+  return file_array.flatten
+end # def get_pathnames_from(an_array, extname='.json')
+
+
+# Display global warnings and errors arrays and exit if necessary
+def abort_if_errors
+  unless $warnings.empty?
+    STDERR.puts
+    STDERR.puts "The following warnings were generated:"
+    STDERR.puts
+    $warnings.each do |w|
+      STDERR.puts "\tWarning: #{w}"
+    end
+    STDERR.print "\nAbort program? (y/N) "
+    answer = (gets).chomp.strip.downcase
+    $errors << "Aborted by user" if answer.size>0 && 'y' == answer[0]
+    $warnings = []
+  end
+  unless $errors.empty?
+    STDERR.puts
+    STDERR.puts "Correct the following errors and try again:"
+    STDERR.puts
+    $errors.each do |e|
+      STDERR.puts "\t#{e}"
+    end
+    STDERR.puts
+    exit(-1)
+  end
+end # def abort_if_errors
+
+
+# Display the usage info
+if  ARGV.empty?               ||
+    ARGV.include?('-h')       ||
+    ARGV.include?('--help')
+  puts usage
+  exit
+end
+
+%w[ -v --verbose ].each do |param|
+  if ARGV.include? param
+    $options[:verbose]        = true
+    ARGV[ ARGV.index(param) ] = nil
+  end
+end
+
+%w[ -d --debug ].each do |param|
+  if ARGV.include? param
+    $options[:debug]          = true
+    ARGV[ ARGV.index(param) ] = nil
+  end
+end
+
+%w[ -o --output ].each do |param|
+  get_out_filename( ARGV.index(param) ) if ARGV.include?(param)
+  unless $options[:out_filename].nil?
+    unless $options[:out_filename].parent.exist?
+      $errors << "Directory does not exist: #{$options[:out_filename].parent}"
+    end
+  end
+end
+
+
+ARGV.compact!
+
+# ...
+
+abort_if_errors
+
+
+######################################################
+# Local methods
+
+
+######################################################
+# Main
+
+at_exit do
+  puts
+  puts "Done."
+  puts
+end
+
+pp $options  if verbose? || debug?
+
+stub = <<EOS
+
+
+   d888888o. 8888888 8888888888 8 8888      88 8 888888888o
+ .`8888:' `88.     8 8888       8 8888      88 8 8888    `88.
+ 8.`8888.   Y8     8 8888       8 8888      88 8 8888     `88
+ `8.`8888.         8 8888       8 8888      88 8 8888     ,88
+  `8.`8888.        8 8888       8 8888      88 8 8888.   ,88'
+   `8.`8888.       8 8888       8 8888      88 8 8888888888
+    `8.`8888.      8 8888       8 8888      88 8 8888    `88.
+8b   `8.`8888.     8 8888       ` 8888     ,8P 8 8888      88
+`8b.  ;8.`8888     8 8888         8888   ,d8P  8 8888    ,88'
+ `Y8888P ,88P'     8 8888          `Y88888P'   8 888888888P
+
+
+EOS
+
+puts stub
+
+
+# __END__
+
+hipchat_s3 = HipchatS3.new(
+  {  :s3 => {
+        :access_key_id => 'your-key',
+        :secret_access_key => 'your-secret',
+        :bucket => 'bucket'
+      },
+      :hipchat => {
+        :api_token => 'your-token'
+      }
+  }
+)
+
+hipchat_s3.s3_bucket = "another_bucket" # you can change your bucket name whenever you need
+
+# displays thumbnail in chat that links to full size image
+hipchat_s3.create_inline_image(
+      'image_path.jpg',
+      hipchat_room_id,
+      {   :thumbnail_path => "thumb_path.jpg",
+          :username => "Magic",
+          :message => "message",
+          :color => "green"
+      }
+)
+
+# displays full size image, no thumbnail, directly in the chat
+hipchat_s3.create_inline_image('image_path.jpg', hipchat_room_id)
+
+# compresses a directory or file and uploads a tar.gz, linked to the room
+hipchat_s3.create_compressed_upload(
+  path_to_compress,
+  hipchat_room_id,
+  {   :message => "Files Compressed",
+      :color => "green"
+  }
+)
+
+# uploads an uncompressed file to s3, displays link in chat
+hipchat_s3.create_file_upload(
+  file_path,
+  hipchat_room_id,
+  {   :username => "FileManager"
+  }
+)
+
+# you can also access the chat object directly
+hipchat_s3.hipchat_client[hipchat_room_id].send(
+  "Enforcer",
+  "Failure! You can't do that!!!",
+  :notify => true,
+  :color => "red"
+)
+
+
+
+
+
+
