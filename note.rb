123ebfff9223c5bdb5293b892e60e0e4bab646e6
Dewayne VanHoozer <dvanhoozer@42six.com>
2016-04-22 17:08:38 -0500

    run up the cwd directory chain looking for a _notes.txt file

--- note.rb
+++ note.rb
@@ -1,8 +1,28 @@
 #!/usr/bin/env ruby
 
 require 'clipboard'
+require 'pathname'
 
-notes_file = "#{ENV['HOME']}/_notes.txt"
+NOTES_FILENAME  = "_notes.txt"
+notes_dir       = Pathname.pwd
+HOME_PATH       = Pathname.new ENV['HOME']
+
+# NOTE: recursive go up the current working directory path looking
+#       for a notes file.  Stop at the $HOME directory; create one
+#       there or use an existing one.
+def get_relevant_notes_file(a_path)
+  if a_path.children.include?(a_path + NOTES_FILENAME)
+    return a_path + NOTES_FILENAME
+  else
+    if a_path == HOME_PATH
+      return HOME_PATH + NOTES_FILENAME
+    else
+      return get_relevant_notes_file a_path.parent
+    end
+  end
+end
+
+notes_file = get_relevant_notes_file Pathname.pwd
 
 File.open(notes_file, "a") do |f|
   f.print "\n--- #{Time.now}\n"
