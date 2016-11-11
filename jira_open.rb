93a7652111bceb64b6af98c3b5a288f89eac787c
Dewayne VanHoozer <dvanhoozer@42six.com>
2016-11-10 23:32:56 -0600

    simple way to open a web browser to a specific JIRA ticket

---
+++ jira_open.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+##########################################################
+###
+##  File: jira_open.rb
+##  Desc: bring up a jira ticket in the browser
+##  By:   Dewayne VanHoozer (dvanhoozer@gmail.com)
+#
+
+if ARGV.empty?
+  puts "ERROR: You need at least one JIRA ticket Number on the command line"
+  exit -1
+end
+
+if ENV['JIRA_BASE_URL'].nil?
+  puts "ERROR: The system environment variable JIRA_BASE_URL is not set."
+  exit -1
+else
+  JIRA_BASE_URL = ENV['JIRA_BASE_URL']
+end
+
+if ENV['JIRA_PROJECT'].nil?
+  puts "ERROR: The system environment variable JIRA_PROJECT is not set."
+  exit -1
+else
+  JIRA_PROJECT = ENV['JIRA_PROJECT']
+end
+
+
+while !ARGV.empty?
+  system("open #{JIRA_BASE_URL}/browse/#{JIRA_PROJECT}-" + ARGV.shift)
+end
