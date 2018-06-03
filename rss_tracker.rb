7bf597b24b343c7f85f76bb79da84bfab0f9c4d8
MadBomber <dvanhoozer@gmail.com>
2018-06-02 20:46:16 -0500

    lots of changes

--- rss_tracker.rb
+++ rss_tracker.rb
@@ -5,15 +5,22 @@
 ###
 ##  File: rss_tracker.rb
 ##  Desc: Track and alert on new items from an RSS feed
+##        Makes use of a rethinkdb cluster
+##        SMELL: looks like its tightly coupled to a specific RSS feed
 ##  By:   Dewayne VanHoozer (dvanhoozer@gmail.com)
 #
 
-require 'simple-rss'
-require 'open-uri'
-require 'htmlentities'
-require 'date'
 
 require 'awesome_print'
+require 'date'
+require 'hashie'
+require 'htmlentities'
+require 'open-uri'
+
+require 'rethinkdb'
+include RethinkDB::Shortcuts
+
+require 'simple-rss'
 
 require 'debug_me'
 include DebugMe
@@ -21,18 +28,22 @@ include DebugMe
 require 'cli_helper'
 include CliHelper
 
-configatron.version = '0.0.1'
+configatron.version = '0.0.2'
 
 HELP = <<EOHELP
 Important:
 
-  Put important stuff here.
+  The "cleanup" option is a one-off used to reformat
+  database entries into the new keys used by the Story
+  class.
 
 EOHELP
 
 cli_helper("__file_description__") do |o|
 
   o.bool    '-a', '--announce', 'Say the title of the article', default: false
+  o.bool    '-c', '--cleanup',  'Cleanup the database',         default: false
+  o.bool    '-s', '--save',     'Save to a database',           default: false
   o.string  '-u', '--url',      'URL for the RSS feed',         default: ENV['INFOWARS_RSS']
 
 end
@@ -48,35 +59,182 @@ end
 
 abort_if_errors
 
-Html = HTMLEntities.new
 
-home    = Pathname.new ENV['HOME']
-configatron.rc_path = home + ".#{my_name.to_s.gsub('.rb','')}rc"
+
+
 
 ######################################################
-# Local methods
+# Local classes/methods
 
-def announce(an_item)
-  text = Html.decode("#{an_item.category}: #{an_item.title}; #{an_item.description}")
-  system("say '#{text}'") if announce?
-  show_ref(an_item) if verbose?
-end
+# An individual story from an RSS feed
+class Story < Hashie::Dash
+  include Hashie::Extensions::Dash::PropertyTranslation
+  include Hashie::Extensions::Dash::Coercion
+  include Hashie::Extensions::IgnoreUndeclared
 
-def show_ref(an_item)
-  puts
-  puts Html.decode(an_item.title)
-  puts an_item.link
-end
+  property :id,       from: 'id'
+  property :title,    from: 'title',    required: true, coerce: ->(value){Html.decode(value)}
+  property :link,     from: 'link',     required: true
+  property :category, from: 'category',   default: 'Other News', coerce: ->(value){Html.decode(value)}
 
-def last_pub_date
-  Time.parse configatron.rc_path.read
-end
+  property :author,   from: 'author',     required: true
+  property :author,   from: :dc_creator,  required: true
+  property :author,   from: 'dc_creator',  required: true
 
-def set_last_pub_date(a_time)
-  rc_file = File.open(configatron.rc_path, 'w')
-  rc_file.puts a_time.to_s
-  rc_file.close
-end
+  property :description,  from: 'description',  required: true, coerce: ->(value){Html.decode(value)}
+
+  property :published_on, from: 'published_on', required: true
+  property :published_on, from: :pubDate,       required: true
+  property :published_on, from: 'pubDate',       required: true
+
+  # Use the system's text to speach capability
+  def announce
+    `say '#{text}'`
+  end # def announce(an_item)
+
+
+  def to_s
+    a_string = <<~EOS
+      Date: #{published_on}
+      #{category}: #{title}
+      #{description}
+      #{link}
+    EOS
+
+    return a_string
+  end
+
+  ######################################################
+  private
+  def text
+    "#{category}: #{title}; #{description}"
+  end
+end # class Story < Hashie::Dash
+
+
+# SMELL: different feeds have different field names
+class RssFeed
+  def initialize(
+      link:   ENV['INFOWARS_RSS'],
+      rc:     nil,
+      host:   'localhost',
+      db:     'infowars',
+      table:  'rss_feed'
+    )
+
+    @rss  = SimpleRSS.parse open(link)
+    @rc   = rc || Pathname.new(ENV['HOME']) +
+                  ".#{__FILE__.split('/').last.gsub('.rb','rc')}"
+
+    begin
+      @conn = r.connect(
+                host:   host,
+                port:   28015,        # default
+                db:     db,
+                table:  table,
+              )
+    rescue Errno::ECONNREFUSED => e
+      STDERR.puts e
+      STDERR.puts "rethinkdb may not be running on host: #{host}"
+      exit(-1)
+    end
+
+    @db     = @conn.use(db)
+    @table  = @db.table(table)
+
+    cleanup if cleanup?
+    get_last_pub_date # from the RC file
+  end # def initialize(....
+
+
+  # process each story in the RSS feed
+  def process
+    # Typical RSS feed starts with the latest story first
+    stories = @rss
+                .items.reverse
+                .map{|item| Story.new(item)}
+                .select{|story| story.published_on > @last_pub_date}
+
+    stories.each do |story|
+      puts "\n" + story.to_s  if verbose?
+      story.announce          if announce?
+      save story              if save?
+
+      set_last_pub_date story.published_on
+    end # stories.each do |story|
+
+    set_last_pub_date stories.last.published_on unless stories.empty?
+  end # def process
+
+
+  ########################################################
+  private
+  # Save an RSS item into the database
+  def save(story)
+    @table.insert(story.to_h).run(@conn) unless duplicate?(story)
+  end
+
+
+  # Get the last publication date from the RC file
+  def get_last_pub_date
+    if @rc.exist?
+      @last_pub_date = Time.parse @rc.read
+    else
+      @last_pub_date = Time.parse "1953-06-03 17:42:16 +0000"
+      set_last_pub_date(@last_pub_date)
+    end
+  end
+
+
+  # Save the last publication date into the RC file
+  def set_last_pub_date(a_time)
+    rc_file = File.open(@rc, 'w')
+    rc_file.puts a_time.to_s
+    rc_file.close
+  end
+
+
+  # Check the database to see if this story is a duplicate
+  def duplicate?(story)
+    # TODO: Assume that story.link is unique
+    #       See if the link is already in the database
+    #       if so, return true, otherwise false
+
+    result = @table.get_all(story.link, index: 'link').count().run(@conn)
+    puts "DUPLICATE" if result > 0
+
+    return result > 0
+  end # def duplicate?(story)
+
+
+  # Before there was class there was raw dumping of RSS items
+  # into the database.  This method reads all documents in the
+  # database and replaces them with the current proper format.
+  def cleanup
+    tot_count = 0
+    bad_count = 0
+    @table.has_fields('pubDate').run(@conn).each do |item|
+      tot_count += 1
+      unless item.has_key? 'pubDate'
+        print 'o'
+        next
+      end
+      bad_count += 1
+      print '.'
+      begin
+        story = Story.new(item)
+      rescue => e
+        STDERR.puts "\n\n" + e.to_s
+        debug_me{[ :item ]}
+      end
+      @table.replace(story.to_h).run(@conn)
+    end
+    print "\n\n"
+    debug_me{[ :tot_count, :bad_count ]}
+  end # def cleanup
+end # class RssFeed
+
+
 
 
 ######################################################
@@ -88,18 +246,8 @@ at_exit do
   puts
 end
 
-unless configatron.rc_path.exist?
-  set_last_pub_date("1953-06-03 17:42:16 +0000")
-end
-
-
-print "Retrieving latest articles ... " if verbose?
-
-rss = SimpleRSS.parse open(ENV['INFOWARS_RSS'])
-
-puts rss.channel.title + ' loaded.' if verbose?
-
 =begin
+# A raw RSS item looks like this:
 {
           :title => "Mysterious Wolf-like Creature Shot in Montana",
            :link => "https://www.infowars.com/mysterious-wolf-like-creature-shot-in-montana/",
@@ -109,12 +257,8 @@ puts rss.channel.title + ' loaded.' if verbose?
      :dc_creator => "Kit Daniels"
 }
 =end
-rss.items.reverse.each do |item|
-  next if item[:pubDate] < last_pub_date
 
-  announce(item)
+Html = HTMLEntities.new
 
-  set_last_pub_date item[:pubDate]
-end # rss.channel.title
-
-
+the_feed = RssFeed.new
+the_feed.process
