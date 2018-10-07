aa4656a64f7d5be2a3062b3630af7fe14859573f
MadBomber <dvanhoozer@gmail.com>
2018-10-06 21:55:40 -0500

    Refactored to use the github_api gem so that means that it only works with github.com hosted repositories.

--- set_upstream.rb
+++ set_upstream.rb
@@ -1,69 +1,48 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
+# frozen_string_literal: true
 ##########################################################
 ###
 ##  File: set_upstream.rb
 ##  Desc: Set the remote upstream value
 ##  By:   Dewayne VanHoozer (dvanhoozer@gmail.com)
 #
+# Uses:
+#   ENV['GITHUB_ACCOUNT']
+#   ENV['GITHUB_API_TOKEN']
+#
+# TODO: add support for bitbucket.com hosted clones
 
 require 'awesome_print'  # Pretty print Ruby objects with proper indentation and colors
+
+require 'github_api'
 
 require 'debug_me'       # A tool to print the labeled value of variables.
 include DebugMe
 
+# SMELL: this is overkill since there is only one parameter
 require 'cli_helper'     # An encapsulation of an integration of slop, nenv, inifile and configatron.
 include CliHelper
 
-configatron.version = '0.0.1'
+configatron.version = '0.0.2'
 
+DEFAULT_LOCALTION_OF_CLONES   =
+  Pathname.new(Nenv.home) + 'sandbox' + 'git_repos' + 'madbomber'
 
-HELP = <<EOHELP
-Important:
+HELP = <<~EOHELP
+  Important:
 
-  The format of the text file must contain lines that
-  are in the pattern:
+    Only works with working directories of clones that are
+    hosted on github.com
 
-    forked from account/repo_name
-
-  Have not found a github API that returns the
-  foked from value.  I created the file by hand
-  using the github.com > my profile > repositories > forks
-  doing a select all and a copy then a paste into a
-  text file.
+    The default --dir path is
+      #{DEFAULT_LOCALTION_OF_CLONES}
 
 EOHELP
 
-cli_helper("Set the upstream remote for github forked clones") do |o|
-
+cli_helper("\nSet the upstream remote for github forked clones") do |o|
   o.path    '--dir',   'Directory containing sub-directories of github-based clones',
-    default: Pathname.new(Nenv.home+'/sandbox/git_repos/madbomber')
-  o.path    '-u', '--upstream',   'Text file of forked from lines',
-    default: Pathname.new('upstream.txt')
-
-end
-
-# Display the usage info
-if  ARGV.empty?
-  show_usage
-  exit
-end
-
-
-# Error check you stuff; use error('some message') and warning('some message')
-
-unless configatron.upstream.exist?
-  error "File does not exist:  #{configatron.upstream}"
-else
-  lines = configatron.upstream.read.split("\n").map{|v|v.chomp.strip}
-  lines.select!{|v| v.start_with?('forked from')}
-  if lines.empty?
-    error "File is not formatted correctly.  See --help"
-  else
-    lines.map! do |a_line|
-      a_line.split.last
-    end
-  end
+    default: DEFAULT_LOCALTION_OF_CLONES
 end
 
 unless configatron.dir.directory?
@@ -76,6 +55,51 @@ abort_if_errors
 ######################################################
 # Local methods
 
+# If the repository cloned into the working directory
+# is forked from someone that is not me, then set the
+# upstream remote to that original repository if its
+# not already set.
+def set_upstream(clone_path, upstream_url)
+  remotes = `cd #{clone_path} && git remote -v`
+  unless remotes.include?('upstream')
+    puts `cd #{clone_path} && git remote add upstream #{upstream_url}`
+  end
+end # def set_upstream(clone_path, upstream_url)
+
+# obtain the github.com hosted repository details
+# of the cloned working directories
+def get_github_repos_already_cloned
+  repos = Array.new
+
+  configatron.dir.children.each do |repo_name|
+    if repo_name.directory?
+      begin
+        repo = Github.repos.get(
+          user:         ENV['GITHUB_ACCOUNT'],
+          oauth_token:  ENV['GITHUB_API_TOKEN'],
+          repo:         repo_name.basename.to_s
+        )
+      rescue Exception => e
+        puts "\n#{e}"
+        next
+      end
+      repos << repo if repo.fork
+    end
+  end # configatron.dir.children.each do |repo_name|
+
+  return repos
+end # def def get_github_repos_already_cloned
+
+# The main() process
+def process_github_clones
+  repos = get_github_repos_already_cloned
+  repos.each do |r|
+    # SMELL: does not work if the working copy directory does
+    #        not match the repo name
+    clone_path = configatron.dir + r.name
+    set_upstream(clone_path, r.parent.html_url)
+  end
+end # def process_github_clones
 
 ######################################################
 # Main
@@ -88,23 +112,5 @@ end
 
 ap configatron.to_h  if verbose? || debug?
 
-lines.each do |upstream|
-  repo_name = upstream.split('/').last
-  repo_path = configatron.dir + repo_name
-  puts
-  print upstream + " ... "
+process_github_clones
 
-  unless repo_path.directory?
-    `cd #{configatron.dir} && git clone https://madbomber@github.com/MadBomber/#{repo_name}.git`
-    print 'CLONED '
-  end
-
-  puts
-
-  remotes = `cd #{repo_path} && git remote -v`
-  puts remotes
-  unless remotes.include?('upstream')
-    puts `cd #{repo_path} && git remote add upstream https://github.com/#{upstream}.git`
-  end
-
-end
