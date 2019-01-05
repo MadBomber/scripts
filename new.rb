17ee9acaace3ddb1b1a3ebb36f432348858760f0
MadBomber <dvanhoozer@gmail.com>
2019-01-04 20:35:20 -0600

    working on integration of rubycritic and rubocop into my sublime text 3 workflow using this program

--- new.rb
+++ new.rb
@@ -1,4 +1,6 @@
 #!/usr/bin/env ruby
+# encoding: utf-8
+# frozen_string_literal: true
 ##########################################################
 ###
 ##  File: new.rb
@@ -20,75 +22,71 @@ my_dir    = me.parent
 my_name   = me.basename.to_s
 
 templates_path = my_dir + 'templates'
-
-templates  = templates_path.children
-
-$options = {
-  verbose:        false
-}
+templates      = templates_path.children
+@verbose       = false
 
 def verbose?
-  $options[:verbose]
+  @verbose
 end
 
-PH_REGEX = /\b(__\S+__)\b/
+PH_REGEX = /\b(__\S+__)\b/.freeze
 
 def find_placeholders(a_string)
   return [] unless a_string.include? '__'
 
   place_holders = a_string.scan(PH_REGEX).flatten.sort.uniq
 
-  debug_me(file: STDERR){[ :place_holders ]}
+  debug_me(file: STDERR) { [ :place_holders ] }
 
   return place_holders
 end
 
-PH_VALUES = Hash.new
+@ph_values = {}
 
 def fill_placeholders(a_string, place_holders)
   text = a_string.dup
 
   place_holders.each do |place_holder|
-    unless PH_VALUES.has_key? place_holder
+    unless @ph_values.key? place_holder
       STDERR.puts "\nEnter value for #{place_holder} then press return:"
-      STDERR.print "> "
-      PH_VALUES[place_holder] = STDIN.gets.chomp
+      STDERR.print '> '
+      @ph_values[place_holder] = STDIN.gets.chomp
     end
-    text.gsub!(place_holder, PH_VALUES[place_holder])
+    text.gsub!(place_holder, @ph_values[place_holder])
   end
   return text
 end
 
-usage = <<EOS
+usage = <<~END_USAGE
 
-Generate a new instance of a file
+  Generate a new instance of a file
 
-Usage: #{my_name} [options] file_template_name+ > new_file_name
+  Usage: #{my_name} [options] file_template_name+ > new_file_name
 
-Where:
+  Where:
 
-  options               Do This
-    -h or --help        Display this message
-    -v or --verbose     Display progress
+    options               Do This
+      -h or --help        Display this message
+      -v or --verbose     Display progress
 
-  file_template_name+   The name(s) of file templates
-                          to concat
+    file_template_name+   The name(s) of file templates
+                            to concat
 
-NOTE:
+  NOTE:
 
-  The output of the selected file template(s) are printed to
-  STDOUT.  Use common file redirection to save to a
-  new_file_name.
+    The output of the selected file template(s) are printed to
+    STDOUT.  Use common file redirection to save to a
+    new_file_name.
 
-  Current file templates are:
+    Current file templates are:
 
-  #{templates.select{|f| f.file?}.map{|f|f.basename.to_s.gsub('.erb','')}.join("\n\t")}
+    #{templates.select(&:file?).map { |f| "\t" + f.basename.to_s.gsub('.erb', '') }.join("\n")}
 
-  Current directory templates are:
+    Current directory templates are:
 
-  #{templates.select{|f| f.directory?}.map{|f|f.basename.to_s}.join("\n\t")}
+    #{templates.select(&:directory?).map { |f| f.basename.to_s }.join("\n\t")}
 
-EOS
+END_USAGE
 
 # Check command line for Problems with Parameters
 errors = []
@@ -100,10 +98,10 @@ if  ARGV.empty?               ||
   exit
 end
 
-%w[ -v --verbose ].each do |param|
+%w[-v --verbose].each do |param|
   if ARGV.include? param
-    $options[:verbose]        = true
-    ARGV[ ARGV.index(param) ] = nil
+    @verbose = true
+    ARGV[ARGV.index(param)] = nil
   end
 end
 
@@ -114,20 +112,20 @@ template_files = ARGV.map { |f| templates_path + f }
 template_files.each_index do |x|
   template_file = template_files[x]
   unless template_file.exist?
-    erb_version = template_file.parent +
-                    (template_file.basename.to_s + '.erb')
-    unless erb_version.exist?
-      errors << "no such template file: #{template_file.basename}"
-    else
-      template_files[x] = erb_version
-    end
+    errors << "no such template file: #{template_file.basename}"
+    next
   end
+
+  erb_version = template_file.parent +
+                (template_file.basename.to_s + '.erb')
+
+  template_files[x] = erb_version if erb_version.exist?
 end
 
 
 unless errors.empty?
   STDERR.puts
-  STDERR.puts "Correct the following errors and try again:"
+  STDERR.puts 'Correct the following errors and try again:'
   STDERR.puts
   errors.each do |e|
     STDERR.puts "\t#{e}"
@@ -139,7 +137,7 @@ end
 
 template_files.each do |template_file|
 
-debug_me(file: STDERR){[ :template_file ]}
+  debug_me(file: STDERR) { [:template_file] }
 
   if template_file.file?
     if '.erb' == template_file.extname.to_s
@@ -154,4 +152,3 @@ debug_me(file: STDERR){[ :template_file ]}
     system "cp -R #{template_file} #{Pathname.pwd}"
   end
 end
-
