b18983dbe78b45c428252e1112cf844a80627e0c
MadBomber <dvanhoozer@gmail.com>
2019-01-13 16:53:31 -0600

    decided to make all of my working directories access the git server with SSH instead of HTTPS

---
+++ change_git_repos_to_ssh_access.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+# change_git_repos_to_ssh_access.rb
+# Edits the 'git config remote.origin.url' from
+# https://<user>@<stuff>
+# to
+# ssh://git@<stuff>
+#
+require 'debug_me'
+include DebugMe
+
+
+FIELD_NAME = 'remote.origin.url'
+SSH_PREFIX = 'ssh://git@'
+
+require 'pathname'  # STDLIB
+
+here = Pathname.pwd
+
+def update_to_ssh(a_directory)
+  return unless (a_directory + '.git').exist?
+  puts "\nUpdating to SSH access for #{a_directory.basename} ..."
+  values = `cd #{a_directory} && git config --get #{FIELD_NAME}`.chomp.split('@')
+  return if values.empty?
+  new_value = SSH_PREFIX + values.last
+  `cd #{a_directory} && git config #{FIELD_NAME} #{new_value}`
+end
+
+
+if (here + '.git').exist?
+  update_to_ssh(here)
+else
+  here.children.each do |dir|
+    update_to_ssh(dir)
+  end
+end
