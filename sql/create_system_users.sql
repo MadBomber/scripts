f160d406a50b2d04fd60bf49e69b3a6be7f1630b
Dewayne VanHoozer <dvanhoozer@gmail.com>
2019-04-17 11:23:28 -0500

    tweated the sql a little

--- sql/create_system_users.sql
+++ sql/create_system_users.sql
@@ -1,6 +1,9 @@
 -- ~/scripts/sql/create_system_users.sql
 -- psql -h localhost -d postgres -f ~/scripts/sql/create_system_users.sql
 
-CREATE USER dewayne   WITH SUPERUSER PASSWORD 'dewayne';
-CREATE USER madbomber WITH SUPERUSER PASSWORD 'madbomber';
-CREATE USER postgres  WITH SUPERUSER PASSWORD 'postgres';
+DROP ROLE IF EXISTS dewayne;
+DROP ROLE IF EXISTS madbomber;
+
+CREATE ROLE dewayne   WITH SUPERUSER PASSWORD 'dewayne';
+CREATE RoLE madbomber WITH SUPERUSER PASSWORD 'madbomber';
+ALTER  ROLE postgres  WITH SUPERUSER PASSWORD 'postgres';
