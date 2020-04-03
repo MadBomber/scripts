f9f1df2d014fb0d78e69e3a26e97d6f90cc91e69
Dewayne VanHoozer <dvanhoozer@gmail.com>
2020-04-03 11:46:59 -0500

    added some quick and dirty options - need to rewrite this wrapper someday

--- erd_for.rb
+++ erd_for.rb
@@ -23,22 +23,34 @@ include DebugMe
 require 'cli_helper'
 include CliHelper
 
-configatron.version = '0.0.1'
+configatron.version = '0.0.2'
 
 HELP = <<EOHELP
 Important:
 
-  Put important stuff here.
+  Use --group to generate one ERD that has all the models listed.
+  Without --group individual ERD files will be created for each model.
+
+  If --group then --title should also be used otherwise the title
+  will be the name of the first model in the list.
+
+  Note that --exclude will not automatically exclude models that are
+  "downstream" from the excluded models.  i.e. the excluded model's
+  relationships.  To exclude those models as well they MUST be in
+  the list of models excluded.
 
 EOHELP
 
 cli_helper("Create ERD files for given database models") do |o|
 
-  o.string  '-t', '--type',    'Type of Output File',              default: 'pdf'
-  o.int     '-r', '--recursion_depth', 'Recursive Depth of Relationships', default: 1
+  o.string  '-t', '--type',    'Type of Output File',                       default: 'pdf'
+  o.int     '-r', '--recursion_depth', 'Recursive Depth of Relationships',  default: 1
+  o.array   '-e', '--exclude', 'Exclude model names seperated by comma',    default: []
   o.array   '-m', '--models',  'Model names seperated by comma'
-  o.path    '-o', '--outdir',  'Directory to save ERD files',      default: DEFAULT_OUT_DIR
-
+  o.string  '-T', '--title',   'Title defaults to Model Name',            default: ''
+  o.boolean '-g', '--group',   'Group models together into one ERD',      default: false
+  o.path    '-o', '--outdir',  'Directory to save ERD files',             default: DEFAULT_OUT_DIR
+  o.string  '-f', '--filename', 'Base name of the output file',           default: ''
 end
 
 # Display the usage info
@@ -80,25 +92,40 @@ base_command = %w[
     --direct
 ].join(' ') + " --only_recursion_depth=#{configatron.recursion_depth}"
 
+base_command += " --exclude #{configatron.exclude.join(',')}," unless configatron.exclude.empty?
+
 
 configatron.models.each do |model|
   command = base_command.dup
 
   puts
   puts "============================================================"
-  puts "Generating ERD for #{model} ... "
 
-  command += " --title='#{model} ERD'"
-  command += " --only='#{model},'"
+  if group?
+    puts "Generating ERD for #{configatron.models.join(', ')} ... "
+    all_models = configatron.models.join(',')
+    command += " --only='#{all_models},'"
+  else
+    puts "Generating ERD for #{model} ... "
+    command += " --only='#{model},'"
+  end
 
-  model_filename = model.snakecase.gsub('/', '_')
+  command += configatron.title.empty? ? " --title='#{model} ERD'" : " --title='#{configatron.title}'"
 
-  command += " --filename=#{configatron.outdir}/#{model_filename}_erd"
+  if configatron.filename.empty?
+    model_filename = model.snakecase.gsub('/', '_')
+    command += " --filename=#{configatron.outdir}/#{model_filename}_erd"
+  else
+    command += " --filename=#{configatron.outdir}/#{configatron.filename}"
+  end
+
   command += " --filetype=#{configatron.type}"
 
   puts "Command:  #{command}"
 
   `#{command}`
+
+  break if group?
 
 end
 
