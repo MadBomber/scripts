372bd4b6859109603986a7a45e9d911f27521cb4
Dewayne VanHoozer <dvanhoozer@gmail.com>
2024-09-16 15:39:59 -0500

    fixed some old issues added when the internal Gemfile was added

--- install_gems_from_file.rb
+++ install_gems_from_file.rb
@@ -5,44 +5,32 @@
 ##  File: install_gems_from_file.rb
 ##  Desc: Take a list of gem names from a file and installs them
 ##  By:   Dewayne VanHoozer (dvanhoozer@gmail.com)
-#
 
-require 'bundler/inline'
+# Ensure that required gems are installed
+`gem install amazing_print debug_me cli_helper`
 
-gemfile true do
-  source 'https://rubygems.org'
-  gem 'amazing_print'
-  gem 'debug_me'
-  gem 'cli_helper'
-end
-
-
-require 'yaml'  # STDLIB
-
-require 'amazing_print'     # Pretty print Ruby objects with proper indentation and colors
-
-require 'debug_me'          # A tool to print the labeled value of variables.
+require 'yaml'                  # STDLIB
+require 'amazing_print'         # Pretty print Ruby objects with proper indentation and colors
+require 'debug_me'              # A tool to print the labeled value of variables.
+require 'cli_helper'            # An encapsulation of an integration of slop, nenv, inifile and configatron.
+require 'set'                   # For storing installed gems
 include DebugMe
-
-require 'cli_helper'        # An encapsulation of an integration of slop, nenv, inifile and configatron.
 include CliHelper
 
 configatron.version = '0.0.2'
 
-cli_helper("return minimum set of gems to install") do |o|
-  o.path    '-f', '--file',       'file containing gem names'
+cli_helper("Return minimum set of gems to install") do |o|
+  o.path    '-f', '--file',       'File containing gem names'
   o.integer '-b', '--batch-size', 'Number of gems to fetch at once', default: 20
 end
 
 # Display the usage info
-if  ARGV.empty?
+if ARGV.empty?
   show_usage
   exit
 end
 
-
-# Error check you stuff; use error('some message') and warning('some message')
-
+# Error check parameters
 if configatron.file.nil?
   error("Argument is missing: -f/--file")
 end
@@ -51,14 +39,10 @@ unless configatron.arguments.empty?
   warning("Unknown arguments ignored: #{configatron.arguments.join(' ')}")
 end
 
-
 abort_if_errors
 
-installed_gems = Gem::Specification.all.map { |gs| gs.name }
-
-######################################################
-# Local methods
-
+# Use Set for faster inclusion check
+installed_gems = Set.new(Gem::Specification.map(&:name))
 
 ######################################################
 # Main
@@ -66,7 +50,7 @@ installed_gems = Gem::Specification.all.map { |gs| gs.name }
 at_exit do
   unless $problem_gems.empty?
     STDERR.puts
-    STDERR.puts "The following gems had problems durning installation:"
+    STDERR.puts "The following gems had problems during installation:"
     STDERR.puts $problem_gems.join('  ')
   end
   STDERR.puts
@@ -74,45 +58,44 @@ at_exit do
   STDERR.puts
 end
 
-ap configatron.to_h  if verbose? || debug?
+ap configatron.to_h if verbose? || debug?
 
+missing_gems = configatron.file.read.split("\n").map(&:strip).uniq.reject(&:empty?) - installed_gems.to_a
 
-missing_gems = configatron.file.read.split("\n").sort.uniq - installed_gems.sort.uniq
-
-$problem_gems = Array.new
+$problem_gems = []
 
 if missing_gems.empty?
   puts "All gems are already installed"
 else
   until missing_gems.empty?
-    gem_names = []
+    gem_names = missing_gems.shift(configatron.batch_size)
 
-    configatron.batch_size.times do
-      gem_names << missing_gems.shift
-    end
+    # Collect the gems that are not installed
+    gem_names.reject! { |gem_name| installed_gems.include?(gem_name) }
 
-    command   = "gem install #{gem_names.join(' ')}"
-    puts command
+    next if gem_names.empty?
+
+    command = "gem install #{gem_names.join(' ')}"
+    puts "Executing: #{command}"
 
     begin
       system command
-
-      begin
-        gem_names.each do |gem_name|
-          depends = YAML.load(`gem spec #{gem_name}`)
-                      .dependencies.map{|d| d.name}
-          unless depends.empty?
-            depends.each {|d| missing_gems.delete d}
-          end
+      gem_names.each do |gem_name|
+        # Check for dependencies of the installed gems
+        begin
+          dependencies = YAML.load(`gem spec #{gem_name}`).dependencies.map(&:name)
+          dependencies.each { |dep| missing_gems.delete(dep) }
+          installed_gems.add(gem_name) # Add to installed gems after successful install
+        rescue Psych::DisallowedClass
+          # NO-OP
+        rescue => e
+          $problem_gems << gem_name
+          STDERR.puts "Could not retrieve dependencies for #{gem_name}: #{e}"
         end
-      rescue Psych::DisallowedClass
-        # NO-OP
       end
-
-    rescue Exception => e
-      $problem_gems << gem_names
-      print "\n...ERROR #{e} ...\n\n"
+    rescue => e
+      $problem_gems << gem_names.join(', ')
+      print "\n...ERROR #{e.message} ...\n\n"
     end
   end # until missing_gems.empty?
 end # if missing_gems.empty?
-
